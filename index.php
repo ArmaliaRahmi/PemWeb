709da9be683d3f1456e68281fe3758d9dd3e1be5
LuthfiaAsh <85048504+LuthfiaAsh@users.noreply.github.com>
2021-06-15 20:55:33 +0700

    Halaman 1

---
+++ index.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" 
+    crossorigin="anonymous">
+
+    <title>Sunan Kalijaga</title>
+  </head>
+  <body>
+
+    <!-- Navbar -->
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="uin1.png" alt="" width="30" height="33" class="d-inline-block align-text-top">
+      Sunan Kalijaga University
+    </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+      data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" 
+      aria-expanded="false" aria-label="Toggle navigation">
+
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="btn d-grid gap-2 d-md-flex justify-content-md-end">
+      <a href="login.php" class="btn btn-primary active me-md-2" aria-current="page">LOGIN</a>
+      </div>
+    </div>
+  </nav>
+
+    <!-- Akhir Navbar -->
+    
+    <br>
+    <div class="container">    
+    <h4 class="text-left">Selamat Datang,</h4><br>
+    </div>
+    <h1 class="text-center">UNIVERSITAS ISLAM NEGERI</h1>
+    <h1 class="text-center">SUNAN KALIJAGA</h1>
+    <h3 class="text-center">YOGYAKARTA</h3><hr>
+
+    <p class="text-center">"Website ini merupakan website Universitas Islam Negeri Sunan Kalijaga Yogyakarta.
+    <br>Website ini memberikan fasilitas kepada para mahasiswanya untuk bisa melihat statistika jumlah mahasiswa. 
+    <br>Tidak hanya itu, website ini juga akan memberikan fasilitas bagi mahasiswa untuk lebih dekat dengan pihak universitas."</p>
+    <br>
+
+    <img src="bg4.jpg" class="img-fluid" alt="...">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" 
+    crossorigin="anonymous"></script>
+
+  </body>
+</html>
